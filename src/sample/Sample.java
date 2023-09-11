0406a382c62d68a40eb1d5d3c59983be8fecdee4
asanoyuuya <asanoyuuya@asanoyuuyanoair>
2023-09-11 21:15:30 +0900

    あ

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -1,10 +1,24 @@
 package sample;
 
+import java.util.Scanner;
+
 public class Sample {
 
 	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
-
+		while(true) {
+			System.out.print("入力をどうぞ：");
+			// TODO 自動生成されたメソッド・スタブ
+			Scanner scanner = new Scanner(System.in);
+			String name = scanner.nextLine();
+			if(!name.matches("^[0-9a-zA-Z]+$")) {
+				System.out.println("半角英数字でない");
+			}else {
+				System.out.println("半角英数字");
+			}
+		}
+	}
+	public static int a() {
+		return 0;
 	}
 
 }
